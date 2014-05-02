d68eafeec827889a547d90a21f55e8bebd3698dd
aviflombaum <avi@flombaum.com>
2014-05-02 13:55:57 -0400

    more work

--- app/assets/javascripts/montages.js
+++ app/assets/javascripts/montages.js
@@ -1,4 +1,29 @@
 $(function(){
   $("div.vine").draggable();
-  $()
+  $("div.vine").resizable({
+    alsoResize: "video",
+    aspectRatio: true,
+  });
+
+  $('.o-slider').slider({ 
+    min: 0, 
+    max: 1, 
+    step: 0.01, 
+    value: 1,
+    orientation: "horizontal",
+      slide: function(e,ui){
+        $(this).parents("div.vine").css('opacity', ui.value)
+      }                
+  });
+
+  $('.z-slider').slider({ 
+    min: 0, 
+    max: 100, 
+    step: 1, 
+    value: 1,
+    orientation: "horizontal",
+      slide: function(e,ui){
+        $(this).parents("div.vine").css('z-index', ui.value)
+      }                
+  });
 });
